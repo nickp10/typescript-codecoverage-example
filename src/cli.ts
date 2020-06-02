1978a446719958c766b20aaa20f89d89db988f24
nickp10 <nickp10@hotmail.com>
2020-06-02 16:09:10 -0600

    Refactor project to use webpack setup instead of gulp

---
+++ src/cli.ts
@@ -0,0 +1,7 @@
+import Person from "./person";
+
+const person = new Person();
+person.firstName = "John";
+person.lastName = "Smith";
+
+console.log(person.getFullName());
---
+++ webpack.common.js
@@ -0,0 +1,54 @@
+const DtsBundleWebpack = require("dts-bundle-webpack");
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        cli: path.resolve(__dirname, "./src/cli.ts"),
+        exports: path.resolve(__dirname, "./src/exports.ts")
+    },
+    externals: [
+        /^[a-z\-0-9]+$/ // Exclude node_modules
+    ],
+    node: {
+        __dirname: false
+    },
+    target: "node",
+    output: {
+        filename: "[name].js",
+        sourceMapFilename: "[name].js.map",
+        library: "typescript-codecoverage-example",
+        libraryTarget: "umd"
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.ts)$/,
+                use: [{
+                    loader: "ts-loader"
+                }]
+            },
+            {
+                test: /\.(js)?$/,
+                use: [{
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true,
+                        presets: ["@babel/preset-env"]
+                    }
+                }]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".ts"]
+    },
+    plugins: [
+        new DtsBundleWebpack({
+            baseDir: "build-dts",
+            main: "build-dts/exports.d.ts",
+            name: "typescript-codecoverage-example",
+            out: "../index.d.ts"
+        })
+    ]
+};
